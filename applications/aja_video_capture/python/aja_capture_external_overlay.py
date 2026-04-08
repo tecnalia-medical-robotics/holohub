a4e68087402e61243a1bc426af38c5714f7b5d1f
Asier Fernandez <asier.fernandez@tecnalia.com>
2026-04-08 19:53:34 +0200

    Apply black formater

--- applications/aja_video_capture/python/aja_capture_external_overlay.py
+++ applications/aja_video_capture/python/aja_capture_external_overlay.py
@@ -42,6 +42,7 @@ def _make_circle_rgba(
     img[mask] = cp.array(color, dtype=cp.uint8)
     return img
 
+
 class CircleImageOp(hs.core.Operator):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -57,7 +58,6 @@ class CircleImageOp(hs.core.Operator):
         spec.param("center_y", 540)
         spec.param("color", (0, 255, 0, 255))
 
-
     def _validate_circle_config(self):
         if self.width <= 0:
             raise ValueError(f"width must be > 0, got {self.width}")
@@ -66,13 +66,9 @@ class CircleImageOp(hs.core.Operator):
         if self.radius < 0:
             raise ValueError(f"radius must be >= 0, got {self.radius}")
         if not 0 <= self.center_x < self.width:
-            raise ValueError(
-                f"center_x must be in [0, {self.width}), got {self.center_x}"
-            )
+            raise ValueError(f"center_x must be in [0, {self.width}), got {self.center_x}")
         if not 0 <= self.center_y < self.height:
-            raise ValueError(
-                f"center_y must be in [0, {self.height}), got {self.center_y}"
-            )
+            raise ValueError(f"center_y must be in [0, {self.height}), got {self.center_y}")
 
     def compute(self, op_input, op_output, context):
         op_input.receive("in")
@@ -93,6 +89,7 @@ class CircleImageOp(hs.core.Operator):
         msg = Entity(context)
         msg.add(hs.as_tensor(img_gpu), self.out_tensor_name)
         op_output.emit(msg, "out")
+
 
 class AJACaptureApp(Application):
     """
@@ -115,13 +112,19 @@ class AJACaptureApp(Application):
             name="circle_generator",
             **self.kwargs("circle_generator"),
         )
-        holoviz_to_videobuffer = HolovizOp(self, name="holoviz_to_videobuffer", **self.kwargs("holoviz_to_videobuffer"))
-        holoviz_aja_consumer = HolovizOp(self, name="holoviz_aja_consumer", **self.kwargs("holoviz_aja_consumer"))
+        holoviz_to_videobuffer = HolovizOp(
+            self, name="holoviz_to_videobuffer", **self.kwargs("holoviz_to_videobuffer")
+        )
+        holoviz_aja_consumer = HolovizOp(
+            self, name="holoviz_aja_consumer", **self.kwargs("holoviz_aja_consumer")
+        )
 
         self.add_flow(source, visualizer, {("video_buffer_output", "receivers")})
         self.add_flow(source, circle_generator, {("video_buffer_output", "in")})
         self.add_flow(circle_generator, holoviz_to_videobuffer, {("out", "receivers")})
-        self.add_flow(holoviz_to_videobuffer, source, {("render_buffer_output", "overlay_buffer_input")})
+        self.add_flow(
+            holoviz_to_videobuffer, source, {("render_buffer_output", "overlay_buffer_input")}
+        )
         self.add_flow(source, holoviz_aja_consumer, {("overlay_buffer_output", "receivers")})
 
 
